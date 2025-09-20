35615383a491c02b55d5b2c6561471d0e97e7636
Pratham Das <prathamdas003@gmail.com>
2025-09-20 23:13:16 +0530

    chore(app): minor file change

--- src/configs/ai-model.ts
+++ src/configs/ai-model.ts
@@ -1,16 +1,6 @@
 import { HuggingFaceTransformersEmbeddings } from "@langchain/community/embeddings/huggingface_transformers";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { env } from "./env.js";
-import { pipeline } from '@huggingface/transformers'
-// import { HuggingFaceInference } from "@langchain/community/llms/hf";
-// import { MistralAIEmbeddings } from "@langchain/mistralai";
-
-//if you are going to change the embeding model then also change the name of the collection otherwise it will corrupt the data in the db
-// export const embeddingsModel = new MistralAIEmbeddings({
-//   model: "mistral-embed",
-//   apiKey:""
-// });
-
 
 export const model = new HuggingFaceTransformersEmbeddings({
   model: "Xenova/all-MiniLM-L6-v2",
@@ -22,6 +12,16 @@ export const mistralModel = new ChatMistralAI({
   apiKey: env.MISTRALAI_API_KEY!,
   maxTokens: 100
 });
+
+// import { pipeline } from '@huggingface/transformers'
+// import { HuggingFaceInference } from "@langchain/community/llms/hf";
+// import { MistralAIEmbeddings } from "@langchain/mistralai";
+
+//if you are going to change the embeding model then also change the name of the collection otherwise it will corrupt the data in the db
+// export const embeddingsModel = new MistralAIEmbeddings({
+//   model: "mistral-embed",
+//   apiKey:""
+// });
 
 // export const audioModel = pipeline(
 //   "text-to-speech",
