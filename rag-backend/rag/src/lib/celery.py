b7c0c0497f57780dee27b32f4263fd7847364d90
Pratham Das <prathamdas003@gmail.com>
2025-10-20 23:27:42 +0530

    feat(app): rag server init

---
+++ rag-backend/rag/src/lib/celery.py
@@ -0,0 +1,14 @@
+from celery import Celery
+
+celery = Celery(
+    "input_files_extraction",
+    broker="redis://localhost:6379/2",
+    backend="redis://localhost:6379/3",
+)
+
+
+celery.autodiscover_tasks(
+    [
+
+    ]
+)
